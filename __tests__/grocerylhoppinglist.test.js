602f99fbfa9914c2ddeb13526f41dd3b37930cdd
timbaron <timothy.akiode@student.aul.edu.ng>
2022-06-26 07:44:09 +0100

    add comments

--- __tests__/grocerylhoppinglist.test.js
+++ __tests__/grocerylhoppinglist.test.js
@@ -2,15 +2,14 @@ import { render, screen, fireEvent } from '@testing-library/react-native';
 import GroceryShoppingList from '../components/groceryshoppinglist'
 
 test('given empty GroceryShoppingList, user can add item to it', () => {
-    render(<GroceryShoppingList />)
+    render(<GroceryShoppingList />) // will render the GroceryShoppingList component
     fireEvent.changeText(
         screen.getByPlaceholderText('Enter grocery item'),
         'Banana'
-    )
+    ) // will change the text to 'Banana' where placeholder is 'Enter grocery item'
 
-    fireEvent.press(screen.getByText('Add the item to list'));
+    fireEvent.press(screen.getByText('Add the item to list')); // will press the button where text is 'Add the item to list'
 
-    const result = screen.getAllByText('Banana');
+    const result = screen.getAllByText('Banana'); // Get all the items where text is 'Banana'
     expect(result).toHaveLength(1); // expect 'banana' to be on the list
-    // expect(result).toHaveLength(1);
 })
